af4353a7693a89fa4c764f4e00e0025227f82ba8
Miltos Stella <miltos.stella@gmail.com>
2021-11-27 10:11:01 -0800

    add initial files for gatsby site initialization

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # miltos.io
-site @ miltos.io generated through gatsby.js
+The site and content for [miltos.io](https://miltos.io), generated through gatsby.js.
+
+## Development
+
+Run `yarn start` to start the local development server.
+
+## Deployment
+
+Deployment of the master branch is automatic via CircleCI and AWS.
+
+## License
+
+This library is released under the [MIT License](LICENSE).
---
+++ gatsby-config.js
@@ -0,0 +1,213 @@
+const siteMetadata = require('./site-metadata.json')
+
+const FileSystem = [
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `blog`,
+      path: `./content/blog`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `galleries`,
+      path: `./content/galleries`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `./content/images`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `data`,
+      path: `./content/data/`,
+    },
+  },
+]
+
+const Styling = [
+  {
+    resolve: `gatsby-plugin-typography`,
+    options: {
+      pathToConfigModule: `src/utils/typography.js`,
+    },
+  },
+  `gatsby-plugin-sass`
+]
+
+const Images = [
+  `gatsby-plugin-sharp`,
+  `gatsby-transformer-sharp`
+]
+
+const Markdown = [
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      gfm: true,
+      commonmark: true,
+      plugins: [
+        'gatsby-remark-autolink-headers',
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            maxWidth: 600,
+            withWebp: true,
+            tracedSVG: true,
+            quality: 50
+          },
+        },
+        {
+          resolve: `gatsby-remark-prismjs`,
+          options: {
+            classPrefix: "language-",
+            inlineCodeMarker: null,
+            aliases: {},
+            showLineNumbers: false,
+            noInlineHighlight: false,
+          },
+        },
+        "gatsby-remark-copy-linked-files"
+      ],
+    },
+  },
+  `gatsby-plugin-catch-links`,
+]
+
+const Blog = [
+  'blog-create-pages',
+  `gatsby-plugin-twitter`
+]
+
+const Feed = [
+  {
+    resolve: `gatsby-plugin-feed`,
+    options: {
+      query: `{
+        site {
+          siteMetadata {
+            title
+            description
+            siteUrl
+            site_url: siteUrl
+          }
+        }
+      }`,
+      feeds: [
+        {
+          query: `
+            {
+              allMarkdownRemark(
+                sort: { order: DESC, fields: [frontmatter___date] }
+                filter: {
+                  frontmatter: { draft: { ne: true } }
+                  fileAbsolutePath: { regex: "content/blog/" }
+                }
+              ) {
+                nodes {
+                  excerpt
+                  html
+                  frontmatter {
+                    title
+                    date
+                  }
+                  fields {
+                    slug
+                  }
+                }
+              }
+            }
+          `,
+          output: `/atom.xml`,
+          title: "Miltos Stella",
+          match: "^/blog/",
+          serialize: ({ query: { site, allMarkdownRemark } }) => {
+            return allMarkdownRemark.nodes.map(node => {
+              return Object.assign({}, node.frontmatter, {
+                description: node.excerpt,
+                date: node.frontmatter.date,
+                url: site.siteMetadata.siteUrl + node.fields.slug,
+                guid: site.siteMetadata.siteUrl + node.fields.slug,
+                custom_elements: [{ "content:encoded": node.html }],
+              })
+            })
+          }
+        },
+      ],
+    },
+  }
+]
+
+const PWA = [
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: "Miltos Stella",
+      short_name: "Stella",
+      start_url: "/",
+      background_color: "#FAFAFA",
+      theme_color: "#FAFAFA",
+      display: "standalone",
+      icon: "content/images/icon.png",
+    },
+  },
+  {
+    resolve: `gatsby-plugin-offline`,
+    options: {
+      navigateFallbackWhitelist: [/\/$/],
+    }
+  }
+]
+
+//const SEO = [
+//  `gatsby-plugin-react-helmet`,
+//  `gatsby-plugin-sitemap`,
+//  {
+//    resolve: `gatsby-plugin-google-analytics`,
+//    options: {
+//      trackingId: `UA-2925354-7`,
+//    },
+//  }
+//]
+
+//const Optimizations = [ 
+//  {
+//    resolve: `gatsby-plugin-netlify-cache`,
+//    options: {
+//      cachePublic: true
+//    }
+//  },
+//  `gatsby-plugin-netlify`,
+//  `gatsby-plugin-force-trailing-slashes`,
+//]
+
+const Data = [
+  `gatsby-transformer-yaml`
+]
+
+const Galleries = [
+  'galleries-create-pages',
+]
+
+module.exports = {
+  siteMetadata,
+  plugins: [
+    ...FileSystem,
+    ...Styling,
+    ...Images,
+    ...Markdown,
+    ...Blog,
+    ...Galleries,
+    ...Data,
+    ...Feed,
+    ...PWA,
+    //...SEO,
+    //...Optimizations,
+  ],
+}
